cdf9aa42f9bb0dd9a1219b4f970375761aede845
seantynan <sean.tynan@gmail.com>
2025-11-11 13:50:47 +0000

    remove old files

--- prompt-tools-ext/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(async (msg) => {
-  if (msg.action === "runPromptlet") {
-    const sidePanel = chrome.runtime.connect({ name: "PromptItChannel" });
-    sidePanel.postMessage(msg);
-  }
-});
